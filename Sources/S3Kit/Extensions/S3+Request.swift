1ce2c351d38779e9685c410baa69b17279362bd3
Abhilash M A <abhidsm@gmail.com>
2020-07-17 16:09:50 +0530

    Fix issue use of undeclared type URLRequest

--- Sources/S3Kit/Extensions/S3+Request.swift
+++ Sources/S3Kit/Extensions/S3+Request.swift
@@ -1,10 +1,13 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 import NIO
 import AsyncHTTPClient
 
 
 extension S3 {
-    
+
     /// Make an S3 request
     func make(request url: URL, method: HTTPMethod, headers: HTTPHeaders, data: Data? = nil, cachePolicy: URLRequest.CachePolicy = .useProtocolCachePolicy) -> EventLoopFuture<HTTPClient.Response> {
         do {
@@ -14,24 +17,24 @@ extension S3 {
             } else {
                 body = nil
             }
-            
+
             var headers = headers
             headers.add(name: "User-Agent", value: "S3Kit-for-Swift")
             headers.add(name: "Accept", value: "*/*")
             headers.add(name: "Connection", value: "keep-alive")
             headers.add(name: "Content-Length", value: String(data?.count ?? 0))
-            
+
             let request = try HTTPClient.Request(
                 url: url.absoluteString,
                 method: method,
                 headers: headers,
                 body: body
             )
-            
+
             return httpClient.execute(request: request, eventLoop: .delegate(on: eventLoop))
         } catch {
             return eventLoop.makeFailedFuture(error)
         }
     }
-    
+
 }
